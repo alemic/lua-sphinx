8efb6aa55489c34fa4ef26dffd5dd9b9959265b0
Lindowx <lindowx@gmail.com>
2013-04-08 09:40:13 +0900

    cp

--- lib/resty/sphinx.lua
+++ lib/resty/sphinx.lua
@@ -1,5 +1,5 @@
 ---
--- Copyright (c) 2003, Zhiqiang Lan (Lindowx). All rights reserved.
+-- Copyright (c) 2013, Zhiqiang Lan (Lindowx). All rights reserved.
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License. You should have
